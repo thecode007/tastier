a1b3cab4235574600bd249de6307314941645197
Abdallah Safieddine <30600335+thecode007@users.noreply.github.com>
2019-10-12 14:10:20 +0300

    Update ApiHelper.java

--- app/src/main/java/com/reflexit/tastier/apiservice/ApiHelper.java
+++ app/src/main/java/com/reflexit/tastier/apiservice/ApiHelper.java
@@ -3,7 +3,7 @@ package com.reflexit.tastier.apiservice;
 public class ApiHelper {
 
     public static String BASE_URL = "https://francecentral.api.cognitive.microsoft.com/face/v1.0/";
-    public static final String OUTH_KEY = "a2af16d9e9144c40bb48d8dd15d1c982";
+    public static final String OUTH_KEY = "******************";
     public final static String DETECT = BASE_URL + "detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_02&returnRecognitionModel=false&detectionModel=detection_01";
     public final static String IDENTIFY = BASE_URL + "identify";
     public final static String ADD_FACE = BASE_URL + "persongroups/tastebuddies/persons/personId/persistedFaces?userData&targetFace&detectionModel=detection_01";
